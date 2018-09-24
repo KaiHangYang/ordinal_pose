182045ddd5777d5fa86e2d52ed1825a49af199e9
kaihang <y907987244@gmail.com>
2018-09-24 11:26:22 +0800

    add the ordinal_3_2

---
+++ net/ordinal_3_2.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+
+from network_utils import mResidualUtils
+import hourglass
+
+class mOrdinal_3_2(object):
+    def __init__(self, nJoints, img_size=256, batch_size=4, is_training=True):
+        self.nJoints = nJoints
+        self.img_size = img_size
+        self.res_utils = mResidualUtils(is_training=is_training)
+        self.is_training = is_training
+        self.batch_size = batch_size
+
+    def build_model(self, input_images):
+        with tf.variable_scope("ordinal_3_2"):
+            first_conv = tf.layers.conv2d(
+                    inputs=input_images,
+                    filters=64,
+                    kernel_size=7,
+                    strides=2,
+                    padding="SAME",
+                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                    name="conv1")
+
+            net = self.res_utils.residual_block(first_conv, 128, name="res2")
+            net = tf.layers.max_pooling2d(net, 2, 2, name="pooling")
+            net = self.res_utils.residual_block(net, 128, name="res3")
+            net = self.res_utils.residual_block(net, 128, name="res4")
+
+            net = hourglass.build_hourglass(net, 256, 4, name="hg_1", is_training=self.is_training)
+
+            features_shape = net.get_shape().as_list()
+            net = tf.reshape(net, [features_shape[0], -1])
+            self.result = tf.contrib.layers.fully_connected(inputs=net, num_outputs=3*self.nJoints, activation_fn=None)
+            self.result = tf.reshape(self.result, [features_shape[0], self.nJoints, 3])
+
+    def build_loss_gt(self, input_coords, lr):
+        self.global_steps = tf.train.get_or_create_global_step()
+        with tf.variable_scope("loss_calculation"):
+            self.loss = tf.nn.l2_loss(input_coords - self.result, name="ordinal_3_2_gt_loss") / self.batch_size
+
+        self.optimizer = tf.train.RMSPropOptimizer(learning_rate=lr)
+        grads_n_vars = self.optimizer.compute_gradients(self.loss)
+        self.train_op = self.optimizer.apply_gradients(grads_n_vars, self.global_steps)
+        tf.summary.scalar("gt_coords_loss", self.loss)
+
+        self.merged_summary = tf.summary.merge_all()
+
+
+    def build_loss_no_gt(self, input_coords_2d, relation_table, loss_table_log, loss_table_pow, lr):
+        self.global_steps = tf.train.get_or_create_global_step()
+
+        with tf.device("/device:GPU:0"):
+            with tf.variable_scope("loss_calculation"):
+                self.loss = 0
+                result_coords_2d = self.result[:, :, 0:2]
+                result_depth = self.result[:, :, 2]
+
+                with tf.variable_scope("rank_loss"):
+                    row_val = tf.tile(result_depth[:, :, tf.newaxis], [1, 1, self.nJoints])
+                    col_val = tf.tile(result_depth[:, tf.newaxis], [1, self.nJoints, 1])
+
+                    rel_distance = (row_val - col_val)
+                    rank_loss = tf.reduce_sum(loss_table_log * tf.log(1 + tf.exp(relation_table * rel_distance)) + loss_table_pow * tf.pow(rel_distance, 2)) / self.batch_size
+
+                with tf.variable_scope("coord2d_loss"):
+                    coord2d_loss = tf.nn.l2_loss(result_coords_2d - input_coords_2d)
+
+                self.loss = coord2d_loss + rank_loss
+
+            with tf.variable_scope("grad"):
+                self.optimizer = tf.train.RMSPropOptimizer(learning_rate=lr)
+                grads_n_vars = self.optimizer.compute_gradients(self.loss)
+                self.train_op = self.optimizer.apply_gradients(grads_n_vars, self.global_steps)
+
+        with tf.device("/cpu:0"):
+            tf.summary.scalar("loss", self.loss)
+            self.merged_summary = tf.summary.merge_all()
---
+++ train/train_3_2_gt.py
@@ -0,0 +1,148 @@
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+import cv2
+import time
+
+sys.path.append("../")
+from net import ordinal_3_1
+from utils.dataread_utils import ordinal_3_1_reader
+from utils.preprocess_utils import ordinal_3_1 as preprocessor
+from utils.visualize_utils import display_utils
+
+##################### Setting for training ######################
+
+nJoints = 17
+batch_size = 4
+img_size = 256
+
+######################## To modify #############################
+train_log_dir = "../logs/train/train_3_1_gt"
+valid_log_dir = "../logs/train/valid_3_1_gt"
+model_dir = "../models"
+model_name = "ordinal_3_1_gt"
+################################################################
+
+is_restore = False
+restore_model_path = ""
+
+valid_iter = 5
+train_iter = 300000
+learning_rate = 2.5e-4
+
+train_img_path = lambda x: "/home/kaihang/DataSet_2/Ordinal/human3.6m/cropped_256/train/images/{}.jpg".format(x)
+train_lbl_path = lambda x: "/home/kaihang/DataSet_2/Ordinal/human3.6m/cropped_256/train/labels/{}.npy".format(x)
+
+valid_img_path = lambda x: "/home/kaihang/DataSet_2/Ordinal/human3.6m/cropped_256/valid/images/{}.jpg".format(x)
+valid_lbl_path = lambda x: "/home/kaihang/DataSet_2/Ordinal/human3.6m/cropped_256/valid/labels/{}.npy".format(x)
+
+#################################################################
+
+if __name__ == "__main__":
+
+    ################### Initialize the data reader ###################
+    train_range = np.arange(0, 312188)
+    valid_range = np.arange(0, 109867)
+
+    train_img_list = [train_img_path(i) for i in train_range]
+    train_lbl_list = [train_lbl_path(i) for i in train_range]
+
+    valid_img_list = [valid_img_path(i) for i in valid_range]
+    valid_lbl_list = [valid_lbl_path(i) for i in valid_range]
+
+    with tf.device('/cpu:0'):
+        train_data_iter, train_data_init_op = ordinal_3_1_reader.get_data_iterator(train_img_list, train_lbl_list, batch_size=batch_size, name="train_reader")
+        valid_data_iter, valid_data_init_op = ordinal_3_1_reader.get_data_iterator(valid_img_list, valid_lbl_list, batch_size=batch_size, name="valid_reader")
+
+    input_images = tf.placeholder(shape=[batch_size, img_size, img_size, 3], dtype=tf.float32)
+    input_depths = tf.placeholder(shape=[batch_size, nJoints], dtype=tf.float32)
+    ordinal_model = ordinal_3_1.mOrdinal_3_1(nJoints, img_size, batch_size, is_training=True)
+
+    with tf.Session() as sess:
+
+        with tf.device("/device:GPU:0"):
+            ordinal_model.build_model(input_images)
+        ordinal_model.build_loss_gt(input_depths, learning_rate)
+
+        print("Network built!")
+
+        train_log_writer = tf.summary.FileWriter(logdir=train_log_dir, graph=sess.graph)
+        valid_log_writer = tf.summary.FileWriter(logdir=valid_log_dir, graph=sess.graph)
+
+        model_saver = tf.train.Saver(max_to_keep=70)
+        net_init = tf.global_variables_initializer()
+
+        sess.run([train_data_init_op, valid_data_init_op, net_init])
+
+        # reload the model
+        if is_restore:
+            if os.path.exists(restore_model_path+".index"):
+                print("#######################Restored all weights ###########################")
+                saver.restore(sess, restore_model_path)
+            else:
+                print("The prev model is not existing!")
+                quit()
+
+
+        is_valid = False
+        valid_count = 0
+
+        while True:
+            global_steps = sess.run(ordinal_model.global_steps)
+
+            if valid_count == valid_iter:
+                valid_count = 0
+                is_valid = True
+            else:
+                valid_count += 1
+                is_valid = False
+
+            # get the data path
+            if is_valid:
+                cur_data_batch = sess.run(valid_data_iter)
+            else:
+                cur_data_batch = sess.run(train_data_iter)
+
+            batch_images_np = np.zeros([batch_size, img_size, img_size, 3], dtype=np.float32)
+            batch_depth_np = np.zeros([batch_size, nJoints], dtype=np.float32)
+
+            # Generate the data batch
+            for b in range(batch_size):
+                cur_img = cv2.imread(cur_data_batch[0][b])
+                cur_label = np.load(cur_data_batch[1][b]).tolist()
+
+                cur_joints = np.concatenate([cur_label["joints_2d"], cur_label["joints_3d"][:, 2][:, np.newaxis]], axis=1)
+                cur_img, cur_joints = preprocessor.preprocess(cur_img, cur_joints)
+
+                batch_depth_np[b] = cur_joints[:, 2] - cur_joints[0, 2] # related to the root
+                batch_images_np[b] = preprocessor.img2train(cur_img, [-1, 1])
+
+                ############### Visualize the augmentated datas
+                # show_img = cur_img.copy().astype(np.uint8)
+                # show_img = display_utils.drawLines(show_img, cur_joints[:, 0:2])
+                # show_img = display_utils.drawPoints(show_img, cur_joints[:, 0:2])
+
+                # print((cur_joints[:, 2] == cur_label["joints_3d"][:, 2]).all())
+
+                # cv2.imshow("img_show", show_img)
+                # cv2.waitKey()
+                ###############################################
+
+            if is_valid:
+                loss, summary  = sess.run([ordinal_model.loss, ordinal_model.merged_summary],
+                        feed_dict={input_images: batch_images_np, input_depths: batch_depth_np})
+                valid_log_writer.add_summary(summary, global_steps)
+            else:
+                _, loss, summary  = sess.run([ordinal_model.train_op, ordinal_model.loss, ordinal_model.merged_summary],
+                        feed_dict={input_images: batch_images_np, input_depths: batch_depth_np})
+                train_log_writer.add_summary(summary, global_steps)
+
+            print("Iteration: {:07d} Loss : {:07f} ".format(global_steps, loss))
+
+            if global_steps % 50000 == 0 and not is_valid:
+                model_saver.save(sess=sess, save_path=os.path.join(model_dir, model_name), global_step=global_steps)
+
+            if global_steps >= train_iter and not is_valid:
+                break
+
