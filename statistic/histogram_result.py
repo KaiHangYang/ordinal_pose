2b900d3779a47aacd3c784a77f837cbff9883bef
kaihang <y907987244@gmail.com>
2018-10-24 07:14:24 +0800

    add the statistic utils

--- statistic/histogram_result.py
+++ statistic/histogram_result.py
@@ -17,24 +17,52 @@ def callback(event, fig, text, data_step, datas):
 
 # 0 mean joints errors for every frames, 1 
 data_type = 0
+use_3d = False
 
 # histogram x steps
+display_step = 20
 data_step = 10
+data_max = 300
 
-eval_result_data = "../evaluation/eval_result/syn/syn_3_1/result_eval_30w.npy"
+# eval_result_datas = [
+        # ("order_3_1", "../evaluation/eval_result/10-22/syn_3_1/result_eval_30w.npy", [0.3, 0.73, 0.22, 0.5]),
+        # ("noorder_3_1", "../evaluation/eval_result/10-23/syn_3_1/result_eval_28w.npy", [0.9, 0.21, 0.07, 0.5]),
+        # ("gt_3_1", "../evaluation/eval_result/10-22/gt_3_1/result_eval_30w.npy", [0.1, 0.21, 0.87, 0.5])
+        # ]
+
+
+eval_result_datas = [
+        ("order_3_2", "../evaluation/eval_result/10-22/syn_3_2/result_eval_26w.npy", [0.3, 0.73, 0.22, 0.5]),
+        ("noorder_3_2", "../evaluation/eval_result/10-23/syn_3_2/result_eval_30w.npy", [0.9, 0.21, 0.07, 0.5]),
+        ("gt_3_2", "../evaluation/eval_result/10-22/gt_3_2/result_eval_30w.npy", [0.1, 0.21, 0.87, 0.5])
+        ]
+
+graph_save_dir = "/home/kaihang/Desktop/test_dir"
 
 if __name__ == "__main__":
-    assert(os.path.isfile(eval_result_data))
 
-    error_s = pose_error.mResultSaver()
-    error_s.restore(eval_result_data)
+    savers = []
+    save_paths = []
+    graph_colors = []
 
-    save_dir = os.path.splitext(eval_result_data)[0]
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    for e in eval_result_datas:
+        assert(os.path.isfile(e[1]))
+        saver = pose_error.mResultSaver(name=e[0])
+        saver.restore(e[1])
+        savers.append(saver)
+        # used for save the graph or statistic datas
+        save_dir = os.path.splitext(e[1])[0]
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
 
-    # error_s.histdatas(os.path.join(save_dir, "mean_dist.npy" if data_type==0 else "per_dist.npy"), data_type=data_type, data_step=data_step)
-    error_s.histogram(save_dir=save_dir, data_type=data_type, data_step=data_step, callback=callback)
+        save_paths.append(os.path.join(save_dir, "mean_dist.npy" if data_type==0 else "per_dist.npy"))
+        graph_colors.append(e[2])
+
+    error_v = pose_error.mResultVisualizer()
+
+    # error_v.histdatas(savers=savers, save_paths=save_paths, data_type=data_type, data_step=data_step)
+    error_v.histogram_by_errors(savers=savers, save_dir=graph_save_dir, colors=graph_colors, data_type=data_type, data_step=data_step, display_step=display_step, data_max=data_max, use_3d=use_3d)
+    # error_v.data_error_line_chart(savers=savers, save_dir=graph_save_dir, colors=graph_colors, data_type=data_type, display_step=display_step)
 
     print("test")
 
--- utils/statistic_utils/pose_error.py
+++ utils/statistic_utils/pose_error.py
@@ -3,6 +3,8 @@ import sys
 import os
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.cm as cm
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
@@ -12,8 +14,9 @@ cur_ax = None
 cur_joint_index = None
 
 class mResultSaver(object):
-    def __init__(self):
+    def __init__(self, name=""):
         self.results = None
+        self.name = name
 
     def restore(self, save_path):
         assert(os.path.isdir(os.path.dirname(save_path)))
@@ -47,41 +50,24 @@ class mResultSaver(object):
 
         self.results.append(cur_result)
 
+class mResultVisualizer(object):
+    def __init__(self):
+        pass
     # data_type, 0 means the error_mean histogram, 1 means the histogram of error_per
-    def histdatas(self, save_path, data_type=0, data_step=5):
-        if self.results is not None:
-            if data_type == 0:
-                # statistic the error distribution of every frame pose
-                statistic_datas = []
+    def histdatas(self, savers, save_paths, data_type=0, data_step=5):
 
-                for i, cur_data in enumerate(self.results):
-                    cur_index = float(cur_data["index"])
-                    cur_mean = cur_data["error_mean"]
-                    statistic_datas.append([cur_mean, cur_index])
+        for s_idx, saver in enumerate(savers):
+            save_path = save_paths[s_idx]
 
-                statistic_datas = np.array(statistic_datas)
-
-                error_dist = statistic_datas[:, 0]
-                bins = np.arange(0, error_dist.max() + data_step, data_step)
-
-                data_arrays = [[] for _ in range(len(bins) - 1)]
-
-                for d in statistic_datas:
-                    data_index = int(d[0] / data_step)
-                    data_arrays[data_index].append(d.copy())
-
-                np.save(save_path, {"hist": np.array(data_arrays), "data_step": data_step})
-            else:
-                # the distribution of each joints error
-                nJoints = len(self.results[0]["error_per"])
-                results = {"data_step": data_step, "hist_arr": []}
-
-                for j_idx in range(nJoints):
+            if saver.results is not None:
+                if data_type == 0:
+                    # statistic the error distribution of every frame pose
                     statistic_datas = []
-                    for cur_data in self.results:
+
+                    for i, cur_data in enumerate(saver.results):
                         cur_index = float(cur_data["index"])
-                        cur_error = cur_data["error_per"][j_idx]
-                        statistic_datas.append([cur_error, cur_index])
+                        cur_mean = cur_data["error_mean"]
+                        statistic_datas.append([cur_mean, cur_index])
 
                     statistic_datas = np.array(statistic_datas)
 
@@ -94,20 +80,45 @@ class mResultSaver(object):
                         data_index = int(d[0] / data_step)
                         data_arrays[data_index].append(d.copy())
 
-                    results["hist_arr"].append(data_arrays)
+                    np.save(save_path, {"hist": np.array(data_arrays), "data_step": data_step})
+                else:
+                    # the distribution of each joints error
+                    nJoints = len(saver.results[0]["error_per"])
+                    results = {"data_step": data_step, "hist_arr": []}
 
-                np.save(save_path, results)
+                    for j_idx in range(nJoints):
+                        statistic_datas = []
+                        for cur_data in saver.results:
+                            cur_index = float(cur_data["index"])
+                            cur_error = cur_data["error_per"][j_idx]
+                            statistic_datas.append([cur_error, cur_index])
 
-    def histogram(self, save_dir, data_type=0, data_step=5, callback=None, fig_size=[14, 10]):
+                        statistic_datas = np.array(statistic_datas)
+
+                        error_dist = statistic_datas[:, 0]
+                        bins = np.arange(0, error_dist.max() + data_step, data_step)
+
+                        data_arrays = [[] for _ in range(len(bins) - 1)]
+
+                        for d in statistic_datas:
+                            data_index = int(d[0] / data_step)
+                            data_arrays[data_index].append(d.copy())
+
+                        results["hist_arr"].append(data_arrays)
+
+                    np.save(save_path, results)
+
+    # this one is with mouse callback
+    def histogram_one(self, saver, save_dir, data_type=0, data_step=5, callback=None, fig_size=[14, 10]):
         global cur_hist_n
         global cur_hist_text
         global cur_ax
 
-        if self.results is not None:
+        if saver.results is not None:
             if data_type == 0:
                 statistic_datas = []
 
-                for i, cur_data in enumerate(self.results):
+                for i, cur_data in enumerate(saver.results):
                     cur_index = float(cur_data["index"])
                     cur_mean = cur_data["error_mean"]
                     statistic_datas.append([cur_mean, cur_index])
@@ -144,7 +155,7 @@ class mResultSaver(object):
                 fig = plt.figure()
                 fig.set_figheight(fig_size[1])
                 fig.set_figwidth(fig_size[0])
-                nJoints = len(self.results[0]["error_per"])
+                nJoints = len(saver.results[0]["error_per"])
 
                 # fig.set_figheight(9) # 6 for histogram, 3 for buttons
                 # fig.set_figwidth(7)
@@ -159,7 +170,7 @@ class mResultSaver(object):
                 for j_idx in range(nJoints):
                     statistic_datas = []
 
-                    for cur_data in self.results:
+                    for cur_data in saver.results:
                         cur_index = float(cur_data["index"])
                         cur_error = cur_data["error_per"][j_idx]
                         statistic_datas.append([cur_error, cur_index])
@@ -215,3 +226,321 @@ class mResultSaver(object):
                 fig.canvas.mpl_connect("key_press_event", save_callback)
 
                 plt.show()
+
+    def histogram(self, savers, save_dir, colors, data_type=0, data_step=5, display_step=10, fig_size=[14, 10], data_max=1000, use_3d=True):
+        global cur_hist_n
+        global cur_hist_text
+        global cur_ax
+
+        for tmp in savers:
+            assert(tmp.results is not None)
+
+        if data_type == 0:
+            statistic_datas = [[] for _ in savers]
+            result_names = []
+
+            for s_idx, saver in enumerate(savers):
+                result_names.append(saver.name)
+                for cur_data in saver.results:
+                    cur_index = float(cur_data["index"])
+                    cur_mean = cur_data["error_mean"]
+                    statistic_datas[s_idx].append([cur_mean, cur_index])
+
+            statistic_datas = np.array(statistic_datas)
+
+            error_dist = statistic_datas[:, :, 0]
+
+            fig = plt.figure()
+            fig.set_figwidth(fig_size[0])
+            fig.set_figheight(fig_size[1])
+
+            if use_3d:
+                ax = fig.add_subplot(1, 1, 1, projection="3d")
+                ax.set_zlabel("The number of datas")
+                ax.set_xticks(np.arange(0, data_max, display_step))
+                ax.set_yticks(np.arange(len(savers)))
+                ax.w_yaxis.set_ticklabels(result_names)
+            else:
+                ax = fig.add_subplot(1, 1, 1)
+                ax.set_xticks(np.arange(0, data_max, display_step))
+                ax.set_ylabel("The number of datas")
+            # text = ax.text(1, 1, 1, '')
+
+            plt.title("Error histogram for mean per frame")
+            ax.set_xlabel("Error(mm)")
+
+            bins = np.arange(0, data_max, data_step)
+
+            hist_arr = []
+
+            for e in error_dist:
+                cur_hist, cur_hist_edge = np.histogram(e, bins=bins, range=(bins.min(), bins.max()), density=False)
+                hist_arr.append(cur_hist)
+
+            hist_arr = np.array(hist_arr)
+            if use_3d:
+                x_data, y_data = np.meshgrid(bins, np.arange(len(hist_arr)) - 0.05)
+
+                x_data = x_data[:, :-1].flatten()
+                y_data = y_data[:, :-1].flatten()
+                z_data = hist_arr.flatten()
+
+                color_arr = []
+                for c_idx in range(hist_arr.shape[0]):
+                    color_arr.append(np.ones([hist_arr.shape[1], 4]) * colors[c_idx])
+
+                color_arr = np.reshape(color_arr, [-1, 4])
+
+                ax.bar3d(x_data, y_data, np.zeros(len(z_data)), data_step, 0.1, z_data, zsort='average', color=color_arr, alpha=0.7)
+            else:
+                for cur_idx, cur_hist in enumerate(hist_arr):
+                    ax.plot(bins[:-1], cur_hist, color=colors[cur_idx], label=result_names[cur_idx])
+                plt.legend()
+
+            def save_callback(event):
+                if event.key == "a":
+                    # save_current fig
+                    extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+                    fig.savefig(os.path.join(save_dir, "frame_mean_error.png"), bbox_inches=extent.expanded(1.2, 1.3))
+
+            fig.canvas.mpl_connect("key_press_event", save_callback)
+            plt.show()
+
+        else:
+            # draw the buttons and add the callback
+            fig = plt.figure()
+            fig.set_figheight(fig_size[1])
+            fig.set_figwidth(fig_size[0])
+            nJoints = len(savers[0].results[0]["error_per"])
+
+            # fig.set_figheight(9) # 6 for histogram, 3 for buttons
+            # fig.set_figwidth(7)
+            fig_grid_row = 10
+            fig_grid_col = 7
+            btn_grid_start = 7
+
+            # ax_hist = plt.subplot2grid((fig_grid_row, fig_grid_col), (0, 0), rowspan=btn_grid_start, colspan=fig_grid_col)
+            btn_arrs = []
+
+            statistic_datas_arr = []
+
+            result_names = []
+            for s_idx, saver in enumerate(savers):
+                result_names.append(saver.name)
+                joints_statistic_datas = []
+                for j_idx in range(nJoints):
+                    statistic_datas = []
+
+                    for cur_data in saver.results:
+                        cur_index = float(cur_data["index"])
+                        cur_error = cur_data["error_per"][j_idx]
+                        statistic_datas.append([cur_error, cur_index])
+
+                    statistic_datas = np.array(statistic_datas)
+                    joints_statistic_datas.append(statistic_datas)
+
+                statistic_datas_arr.append(joints_statistic_datas)
+
+            statistic_datas_arr = np.array(statistic_datas_arr)
+
+            def btn_callback(event):
+                global cur_ax
+                global cur_joint_index
+
+                joint_index = int(event.inaxes.get_children()[0].get_text().split()[1]) - 1
+                cur_joint_index = joint_index
+
+                statistic_datas = statistic_datas_arr[:, joint_index]
+                error_dist = statistic_datas[:, :, 0]
+
+                if use_3d:
+                    ax_hist = plt.subplot2grid((fig_grid_row, fig_grid_col), (0, 0), rowspan=btn_grid_start-1, colspan=fig_grid_col, projection="3d")
+                    cur_ax = ax_hist
+                    ax_hist.set_zlabel("The number of datas")
+
+                    ax_hist.set_xticks(np.arange(0, data_max, display_step))
+                    ax_hist.set_yticks(np.arange(len(error_dist)))
+                    ax_hist.w_yaxis.set_ticklabels(result_names)
+                else:
+                    ax_hist = plt.subplot2grid((fig_grid_row, fig_grid_col), (0, 0), rowspan=btn_grid_start-1, colspan=fig_grid_col)
+                    cur_ax = ax_hist
+
+                    ax_hist.set_xticks(np.arange(0, data_max, display_step))
+                    ax_hist.set_ylabel("The number of datas")
+
+                # text = ax_hist.text(1, 1, '')
+                ax_hist.set_xlabel("Error(mm)")
+
+                bins = np.arange(0, data_max, data_step)
+                hist_arr = []
+
+                for e in error_dist:
+                    cur_hist, cur_hist_edge = np.histogram(e, bins=bins, range=(bins.min(), bins.max()), density=False)
+                    hist_arr.append(cur_hist)
+
+                if use_3d:
+                    x_data, y_data = np.meshgrid(bins, np.arange(len(hist_arr)) - 0.05)
+
+                    hist_arr = np.array(hist_arr)
+                    x_data = x_data[:, :-1].flatten()
+                    y_data = y_data[:, :-1].flatten()
+                    z_data = hist_arr.flatten()
+
+                    color_arr = []
+                    for c_idx in range(hist_arr.shape[0]):
+                        color_arr.append(np.ones([hist_arr.shape[1], 4]) * colors[c_idx])
+
+                    color_arr = np.reshape(color_arr, [-1, 4])
+
+                    ax_hist.bar3d(x_data, y_data, np.zeros(len(z_data)), data_step, 0.1, z_data, zsort='average', color=color_arr, alpha=0.7)
+                else:
+                    for cur_idx, cur_hist in enumerate(hist_arr):
+                        ax_hist.plot(bins[:-1], cur_hist, color=colors[cur_idx], label=result_names[cur_idx])
+                    plt.legend()
+
+                fig.canvas.draw()
+
+            def save_callback(event):
+                global cur_ax
+                global cur_joint_index
+                if event.key == "a":
+                    # save_current fig
+                    extent = cur_ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+                    fig.savefig(os.path.join(save_dir, "joint_{}_error.png".format(cur_joint_index)), bbox_inches=extent.expanded(1.2, 1.3))
+
+
+            for j_idx in range(nJoints):
+                ax_btns = plt.subplot2grid((fig_grid_row, fig_grid_col), (btn_grid_start + j_idx/fig_grid_col, j_idx%fig_grid_col), rowspan=1, colspan=1)
+                btn_arrs.append(Button(ax_btns, "Joint {}".format(j_idx + 1)))
+                str_to_print = str(j_idx)
+
+                btn_arrs[j_idx].on_clicked(btn_callback)
+
+            fig.canvas.mpl_connect("key_press_event", save_callback)
+            plt.show()
+
+    def data_error_line_chart(self, savers, save_dir, colors, data_type=0, display_step=10, fig_size=[14, 10]):
+        global cur_hist_n
+        global cur_hist_text
+        global cur_ax
+
+        for tmp in savers:
+            assert(tmp.results is not None)
+
+        nJoints = len(savers[0].results[0]["error_per"])
+        nDatas = len(savers[0].results)
+
+        if data_type == 0:
+            statistic_datas = [[] for _ in savers]
+            result_names = []
+
+            for s_idx, saver in enumerate(savers):
+                result_names.append(saver.name)
+                for cur_data in saver.results:
+                    cur_index = float(cur_data["index"])
+                    cur_mean = cur_data["error_mean"]
+                    statistic_datas[s_idx].append([cur_mean, cur_index])
+
+            statistic_datas = np.array(statistic_datas)
+
+            fig = plt.figure()
+            fig.set_figwidth(fig_size[0])
+            fig.set_figheight(fig_size[1])
+
+            ax = fig.add_subplot(1, 1, 1)
+            ax.set_xticks(np.arange(0, nDatas, display_step))
+            ax.set_ylabel("Error(mm)")
+            # text = ax.text(1, 1, 1, '')
+
+            plt.title("Error distribution of all datas")
+            ax.set_xlabel("data index")
+
+            for cur_idx, cur_data in enumerate(statistic_datas):
+                ax.plot(cur_data[:, 1], cur_data[:, 0], color=colors[cur_idx], label=result_names[cur_idx])
+            plt.legend()
+
+            def save_callback(event):
+                if event.key == "a":
+                    # save_current fig
+                    extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+                    fig.savefig(os.path.join(save_dir, "frame_mean_error.png"), bbox_inches=extent.expanded(1.2, 1.3))
+
+            fig.canvas.mpl_connect("key_press_event", save_callback)
+            plt.show()
+
+        else:
+            # draw the buttons and add the callback
+            fig = plt.figure()
+            fig.set_figheight(fig_size[1])
+            fig.set_figwidth(fig_size[0])
+
+            # fig.set_figheight(9) # 6 for histogram, 3 for buttons
+            # fig.set_figwidth(7)
+            fig_grid_row = 10
+            fig_grid_col = 7
+            btn_grid_start = 7
+
+            # ax_hist = plt.subplot2grid((fig_grid_row, fig_grid_col), (0, 0), rowspan=btn_grid_start, colspan=fig_grid_col)
+            btn_arrs = []
+
+            statistic_datas_arr = []
+
+            result_names = []
+            for s_idx, saver in enumerate(savers):
+                result_names.append(saver.name)
+                joints_statistic_datas = []
+                for j_idx in range(nJoints):
+                    statistic_datas = []
+
+                    for cur_data in saver.results:
+                        cur_index = float(cur_data["index"])
+                        cur_error = cur_data["error_per"][j_idx]
+                        statistic_datas.append([cur_error, cur_index])
+
+                    statistic_datas = np.array(statistic_datas)
+                    joints_statistic_datas.append(statistic_datas)
+
+                statistic_datas_arr.append(joints_statistic_datas)
+
+            statistic_datas_arr = np.array(statistic_datas_arr)
+
+            def btn_callback(event):
+                global cur_ax
+                global cur_joint_index
+
+                joint_index = int(event.inaxes.get_children()[0].get_text().split()[1]) - 1
+                cur_joint_index = joint_index
+
+                statistic_datas = statistic_datas_arr[:, joint_index]
+
+                ax = plt.subplot2grid((fig_grid_row, fig_grid_col), (0, 0), rowspan=btn_grid_start-1, colspan=fig_grid_col)
+                cur_ax = ax
+
+                ax.set_xticks(np.arange(0, nDatas, display_step))
+                ax.set_xlabel("data index")
+                ax.set_ylabel("Error(mm)")
+
+                for cur_idx, cur_data in enumerate(hist_arr):
+                    ax.plot(cur_data[:, 1], cur_data[:, 0], color=colors[cur_idx], label=result_names[cur_idx])
+                plt.legend()
+
+                fig.canvas.draw()
+
+            def save_callback(event):
+                global cur_ax
+                global cur_joint_index
+                if event.key == "a":
+                    # save_current fig
+                    extent = cur_ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+                    fig.savefig(os.path.join(save_dir, "joint_{}_error.png".format(cur_joint_index)), bbox_inches=extent.expanded(1.2, 1.3))
+
+
+            for j_idx in range(nJoints):
+                ax_btns = plt.subplot2grid((fig_grid_row, fig_grid_col), (btn_grid_start + j_idx/fig_grid_col, j_idx%fig_grid_col), rowspan=1, colspan=1)
+                btn_arrs.append(Button(ax_btns, "Joint {}".format(j_idx + 1)))
+                str_to_print = str(j_idx)
+
+                btn_arrs[j_idx].on_clicked(btn_callback)
+
+            fig.canvas.mpl_connect("key_press_event", save_callback)
+            plt.show()
