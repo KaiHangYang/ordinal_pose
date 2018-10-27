fe19a3e3adace229c0f7c0dc8dffbf94090a5338
kaihang <y907987244@gmail.com>
2018-10-28 01:48:13 +0800

    checked the syn scripts

---
+++ debug/check_syn_draw.py
@@ -0,0 +1,38 @@
+import numpy as np
+import os
+import sys
+import cv2
+import time
+
+sys.path.append("../")
+
+from utils.preprocess_utils import syn_preprocess
+
+if __name__ == "__main__":
+
+    label_dir_fn = lambda x: os.path.join("/home/kaihang/DataSet_2/Ordinal/human3.6m/cropped_256/train/labels_syn", x)
+    image_dir_fn = lambda x: os.path.join("/home/kaihang/DataSet_2/Ordinal/human3.6m/cropped_256/train/images_syn_64x64", x)
+    real_image_dir_fn = lambda x: os.path.join("/home/kaihang/DataSet_2/Ordinal/human3.6m/cropped_256/train/images", x)
+
+    label_lists = os.listdir(label_dir_fn(""))
+
+    for cur_label_name in label_lists:
+        cur_label = np.load(label_dir_fn(cur_label_name)).tolist()
+        cur_image = cv2.imread(image_dir_fn("{}.jpg".format(cur_label_name.split(".")[0])))
+        cur_real_image = cv2.imread(real_image_dir_fn("{}.jpg".format(cur_label_name.split(".")[0])))
+
+        joints_2d = cur_label["joints_2d"]
+        bone_status = cur_label["bone_status"]
+        bone_order = cur_label["bone_order"]
+
+        SYN_TIME = time.time()
+        synmap, sep_synmaps = syn_preprocess.draw_syn_img(joints_2d, bone_status, bone_order)
+        SYN_TIME = time.time() - SYN_TIME
+
+        cv2.imshow("painted_img", cv2.resize(cur_image, (256, 256)))
+        cv2.imshow("synmap", cv2.resize(synmap, (256, 256)))
+        cv2.imshow("real_img", cur_real_image)
+        cv2.waitKey()
+        print("SYN_TIME", SYN_TIME)
+
+        print(cur_label["source"])
--- utils/preprocess_utils/syn_preprocess.py
+++ utils/preprocess_utils/syn_preprocess.py
@@ -9,49 +9,133 @@ import common
 flip_array = np.array([[11, 14], [12, 15], [13, 16], [1, 4], [2, 5], [3, 6]])
 
 # currently I don't augment the data
-# def preprocess(img, annots, is_training=True, is_rotate=True):
-def preprocess(img, syn_img, annots):
-    # settings = {
-        # "img_size": 256,
-        # "crop_box_size": 256,
-        # "num_of_joints": 17,
-        # "scale_range": 0.25,# max is 0.5
-        # "rotate_range": 30.0 if is_rotate else 0.0, # max 45
-        # "shift_range": 0, # pixel
-        # "is_flip": 1,
-        # "pad_color": [0.2, 0.2, 0.2],
-        # "flip_array": flip_array
-    # }
+def preprocess(img, annots, is_training=True):
+    settings = {
+        "img_size": 256,
+        "crop_box_size": 256,
+        "num_of_joints": 17,
+        "scale_range": 0.1,# max is 0.5 no scale now
+        "rotate_range": 30.0, # max 45
+        "shift_range": 0, # pixel
+        "is_flip": 1,
+        "pad_color": [0.5, 0.5, 0.5],
+        "flip_array": flip_array
+    }
 
     img = img.astype(np.float32).copy()
-    syn_img = syn_img.astype(np.float32).copy()
 
     # normalize to [0, 1.0]
     if np.max(img) > 2:
         img = img / 255.0
 
-    if np.max(syn_img) > 2:
-        syn_img = syn_img / 255.0
+    if is_training:
+        # augment the color of the images
+        img[:, :, 0] *= np.random.uniform(0.8, 1.2)
+        img[:, :, 1] *= np.random.uniform(0.8, 1.2)
+        img[:, :, 2] *= np.random.uniform(0.8, 1.2)
+        img = np.clip(img, 0.0, 1.0)
 
-    # if is_training:
-        # # augment the color of the images
-        # img[:, :, 0] *= np.random.uniform(0.8, 1.2)
-        # img[:, :, 1] *= np.random.uniform(0.8, 1.2)
-        # img[:, :, 2] *= np.random.uniform(0.8, 1.2)
-        # img = np.clip(img, 0.0, 1.0)
+        aug_img, aug_annot = augment_data_2d(img, annots, settings)
+    else:
+        aug_img = img
+        aug_annot = annots
 
-        # pad_size = img.shape[0] / 2
-
-        # aug_img, aug_annot = augment_data_2d(img, annots, settings)
-    # else:
-        # aug_img = img
-        # aug_annot = annots
-
-    # return aug_img, aug_annot
-    return img, syn_img, annots
+    return aug_img, aug_annot
 
 def flip_data(img, annots, size=64):
     return common._flip_data(img, annots, flip_array, size)
 
 def flip_annot(annots, size=64):
     return common._flip_annot(annots, flip_array, size=size)
+
+# draw the ground truth joints_2d
+bones_indices = np.array([
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [0, 4],
+        [4, 5],
+        [5, 6],
+        [0, 7],
+        [7, 8],
+        [8, 9],
+        [9, 10],
+        [8, 11],
+        [11, 12],
+        [12, 13],
+        [8, 14],
+        [14, 15],
+        [15, 16]
+])
+
+
+bone_colors = np.array([
+    [1.000000, 1.000000, 0.000000],
+    [0.492543, 0.000000, 0.000000],
+    [0.000000, 1.000000, 0.000000],
+    [1.000000, 0.349454, 0.000000],
+    [0.499439, 0.558884, 1.000000],
+    [0.000000, 0.362774, 0.000000],
+    [0.500312, 0.000000, 0.624406],
+    [0.501744, 0.724322, 0.275356],
+    [0.000000, 1.000000, 1.000000],
+    [1.000000, 0.000000, 1.000000],
+    [1.000000, 0.499433, 0.611793],
+    [1.000000, 0.800000, 1.000000],
+    [0.000000, 0.502502, 0.611632],
+    [0.200000, 0.700000, 0.300000],
+    [0.700000, 0.300000, 0.100000],
+    [0.300000, 0.200000, 0.800000]
+])
+
+def draw_syn_img(joints_2d, bone_status, bone_order, size=64, bg_color=0.2, bone_width=1, joint_ratio=1):
+
+    assert(joints_2d.max() < size)
+
+    bone_sum = bone_status.shape[0]
+    bg_color = int(bg_color * 255)
+
+    sep_synmaps = np.array([bg_color * np.ones([size, size, 3]) for _ in range(bone_sum)]).astype(np.uint8)
+    synmap = bg_color * np.ones([size, size, 3]).astype(np.uint8)
+
+    for cur_bone in bone_order:
+        ####### get bone informations first #######
+        cur_bone_color = np.array([bone_colors[cur_bone][2] * 255, bone_colors[cur_bone][1] * 255, bone_colors[cur_bone][0] * 255])
+
+        if bone_status[cur_bone] == 0:
+            # not certain
+            cur_joint_color = (127, 127, 127)
+        elif bone_status[cur_bone] == 1:
+            # forward
+            cur_joint_color = (255, 255, 255)
+        else:
+            # backward
+            cur_joint_color = (0, 0, 0)
+
+        source_joint = joints_2d[bones_indices[cur_bone][0]]
+        target_joint = joints_2d[bones_indices[cur_bone][1]]
+        ###########################################
+
+        dir_2d = (target_joint - source_joint) / (0.0000001 + np.linalg.norm(target_joint - source_joint))
+        w_vec = np.array([dir_2d[1], -dir_2d[0]])
+
+        joint_to_draw = tuple(np.round(target_joint).astype(np.int32))
+
+        vertex_1 = source_joint - bone_width / 2.0 * w_vec;
+        vertex_2 = source_joint + bone_width / 2.0 * w_vec;
+
+        vertex_3 = target_joint + bone_width / 2.0 * w_vec;
+        vertex_4 = target_joint - bone_width / 2.0 * w_vec;
+
+        vertices = np.array([[np.round(vertex_1[0]), np.round(vertex_1[1])],
+                             [np.round(vertex_2[0]), np.round(vertex_2[1])],
+                             [np.round(vertex_3[0]), np.round(vertex_3[1])],
+                             [np.round(vertex_4[0]), np.round(vertex_4[1])]]).astype(np.int32)
+
+        synmap = cv2.fillConvexPoly(synmap, vertices, cur_bone_color, cv2.LINE_AA);
+        synmap = cv2.circle(synmap, joint_to_draw, joint_ratio, cur_joint_color, cv2.FILLED, cv2.LINE_AA);
+
+        sep_synmaps[cur_bone] = cv2.fillConvexPoly(sep_synmaps[cur_bone], vertices, cur_bone_color, cv2.LINE_AA);
+        sep_synmaps[cur_bone] = cv2.circle(sep_synmaps[cur_bone], joint_to_draw, joint_ratio, cur_joint_color, cv2.FILLED, cv2.LINE_AA);
+
+    return synmap, sep_synmaps
