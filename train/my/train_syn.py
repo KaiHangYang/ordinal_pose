0c8f110a0bf2d809b03dbd50e40948ea2074a4ce
kaihang <y907987244@gmail.com>
2018-11-04 23:52:21 +0800

    modified the train_syn.py scripts for fb and br classification

--- train/my/train_syn.py
+++ train/my/train_syn.py
@@ -26,7 +26,7 @@ valid_log_dir = os.path.join(configs.log_dir, "valid")
 if not os.path.exists(configs.model_dir):
     os.makedirs(configs.model_dir)
 
-restore_model_iteration = 220000
+restore_model_iteration = None
 #################################################################
 
 if __name__ == "__main__":
@@ -49,20 +49,23 @@ if __name__ == "__main__":
         valid_data_iter, valid_data_init_op = syn_reader.get_data_iterator(valid_img_list, valid_lbl_list, batch_size=configs.valid_batch_size, name="valid_reader", is_shuffle=False)
 
     input_images = tf.placeholder(shape=[None, configs.img_size, configs.img_size, 3], dtype=tf.float32, name="input_images")
+    input_center_2d = tf.placeholder(shape=[None, config.nJoints, 2], dtype=tf.float32, name="input_center_2d")
 
-    input_sep_synmaps = tf.placeholder(shape=[None, configs.sep_syn_img_size, configs.sep_syn_img_size, 3*(configs.nJoints - 1)], dtype=tf.float32, name="input_sep_synmaps")
-    input_synmap = tf.placeholder(shape=[None, configs.syn_img_size, configs.syn_img_size, 3], dtype=tf.float32, name="input_synmap")
+    # both the fb_info and br_info are one-hot arrays
+    input_fb_info = tf.placeholder(shape=[None, config.nJoints-1, 3], dtype=tf.float32, name="input_fb_info")
+    input_br_info = tf.placeholder(shape=[None, (config.nJoints-1) * (config.nJoints-2) / 2, 3], name="input_br_info") # up-triangle of the matrix
 
     input_is_training = tf.placeholder(shape=[], dtype=tf.bool, name="input_is_training")
     input_batch_size = tf.placeholder(shape=[], dtype=tf.float32, name="input_batch_size")
 
-    syn_model = syn_net.mSynNet(nJoints=configs.nJoints, img_size=configs.img_size, batch_size=input_batch_size, is_training=input_is_training, loss_weight_sep_synmaps=10.0, loss_weight_synmap=1.0)
+    syn_model = syn_net.mSynNet(nJoints=configs.nJoints, img_size=configs.img_size, batch_size=input_batch_size, is_training=input_is_training, loss_weight_heatmaps=1.0, loss_weight_fb=1.0, loss_weight_br=1.0)
 
     with tf.Session() as sess:
         with tf.device("/device:GPU:0"):
             syn_model.build_model(input_images)
+            input_heatmaps = syn_model.build_input_heatmaps(input_center_2d, name="input_heatmaps", stddev=2.0, gaussian_coefficient=False)
 
-        syn_model.build_loss(input_sep_synmaps=input_sep_synmaps, input_synmap=input_synmap, lr=configs.learning_rate, lr_decay_step=configs.lr_decay_step, lr_decay_rate=configs.lr_decay_rate)
+        syn_model.build_loss(input_heatmaps=input_heatmaps, input_fb=input_fb_info, input_br=input_br_info, lr=configs.learning_rate, lr_decay_step=configs.lr_decay_step, lr_decay_rate=configs.lr_decay_rate)
 
         print("Network built!")
         train_log_writer = tf.summary.FileWriter(logdir=train_log_dir, graph=sess.graph)
@@ -105,9 +108,9 @@ if __name__ == "__main__":
 
             batch_size = len(cur_data_batch[0])
             batch_images_np = np.zeros([batch_size, configs.img_size, configs.img_size, 3], dtype=np.float32)
-
-            batch_synmap_np = np.zeros([batch_size, configs.syn_img_size, configs.syn_img_size, 3], dtype=np.float32)
-            batch_sep_synmaps_np = np.zeros([batch_size, configs.sep_syn_img_size, configs.sep_syn_img_size, 3 * (configs.nJoints - 1)], dtype=np.float32)
+            batch_center_2d = np.zeros([batch_size, config.nJoints, 2], dtype=np.float32)
+            batch_fb_info = np.zeros([batch_size, config.nJoints-1, 3], dtype=np.float32)
+            batch_br_info = np.zeros([batch_size, (config.nJoints-1)*(config.nJoints-2) / 2, 3], dtype=np.float32)
 
             # Generate the data batch
             img_path_for_show = [[] for i in range(max(configs.train_batch_size, configs.valid_batch_size))]
@@ -120,72 +123,54 @@ if __name__ == "__main__":
                 cur_img = cv2.imread(cur_data_batch[0][b])
                 cur_label = np.load(cur_data_batch[1][b]).tolist()
 
-                # the joints_2d in the label_syn is resize in [64, 64]
-
-                cur_joints_2d = cur_label["joints_2d"].copy() * configs.joints_2d_scale
+                cur_joints_2d = cur_label["joints_2d"].copy()
                 cur_bone_status = cur_label["bone_status"].copy()
-                cur_bone_order = cur_label["bone_order"].copy()
+                cur_bone_relations = cur_label["bone_relations"].copy()
 
-                cur_img, cur_joints_2d, cur_bone_status, cur_bone_order = syn_preprocess.preprocess(img=cur_img, joints_2d=cur_joints_2d, bone_status=cur_bone_status, bone_order=cur_bone_order, is_training=not is_valid)
+                cur_img, cur_joints_2d, cur_bone_status, cur_bone_relations = syn_preprocess.preprocess(img=cur_img, joints_2d=cur_joints_2d, bone_status=cur_bone_status, bone_relations=cur_bone_relations, is_training=not is_valid)
 
-                cur_joints_2d = cur_joints_2d
-
-                # draw the synsetic imgs as the ground truth
-                cur_synmap, cur_sep_synmaps = syn_preprocess.draw_syn_img(cur_joints_2d, cur_bone_status, cur_bone_order, size=configs.syn_img_size, sep_size=configs.sep_syn_img_size, bone_width=4, joint_ratio=4)
                 batch_images_np[b] = cur_img
-
-                # make them [0, 1]
-                batch_sep_synmaps_np[b] = np.concatenate(cur_sep_synmaps, axis=2) / 255.0
-                batch_synmap_np[b] = cur_synmap / 255.0
+                batch_center_2d[b] = np.round(cur_joints_2d / configs.joints_2d_scale)
+                batch_fb_info[b] = np.eye(3)[cur_bone_status]
+                batch_br_info[b] = np.eye(3)[cur_bone_relations[np.triu_indices(configs.nJoints-1, k=1)]] # only get the upper triangle
 
             if is_valid:
                 loss, \
-                sep_synmaps_loss, \
-                synmap_loss, \
+                heatmaps_loss, \
+                fb_loss, \
+                br_loss, \
                 lr, \
                 summary  = sess.run(
                         [
                          syn_model.total_loss,
-                         syn_model.sep_synmaps_loss,
-                         syn_model.synmap_loss,
+                         syn_model.heatmaps_loss,
+                         syn_model.fb_loss,
+                         syn_model.br_loss,
                          syn_model.lr,
                          syn_model.merged_summary],
-                        feed_dict={input_images: batch_images_np, input_sep_synmaps: batch_sep_synmaps_np, input_synmap: batch_synmap_np, input_is_training: False, input_batch_size: configs.valid_batch_size})
+                        feed_dict={input_images: batch_images_np, input_center_2d: batch_center_2d, input_fb_info: batch_fb_info, input_br_info: batch_br_info, input_is_training: False, input_batch_size: configs.valid_batch_size})
                 valid_log_writer.add_summary(summary, global_steps)
             else:
-                if global_steps % write_log_iter == 0:
-                    _, \
-                    loss,\
-                    sep_synmaps_loss, \
-                    synmap_loss, \
-                    lr,\
-                    summary  = sess.run(
-                            [
-                             syn_model.train_op,
-                             syn_model.total_loss,
-                             syn_model.sep_synmaps_loss,
-                             syn_model.synmap_loss,
-                             syn_model.lr,
-                             syn_model.merged_summary],
-                            feed_dict={input_images: batch_images_np, input_sep_synmaps: batch_sep_synmaps_np, input_synmap: batch_synmap_np, input_is_training: True, input_batch_size: configs.train_batch_size})
-                    train_log_writer.add_summary(summary, global_steps)
-                else:
-                    _, \
-                    loss,\
-                    sep_synmaps_loss, \
-                    synmap_loss, \
-                    lr = sess.run(
-                            [
-                             syn_model.train_op,
-                             syn_model.total_loss,
-                             syn_model.sep_synmaps_loss,
-                             syn_model.synmap_loss,
-                             syn_model.lr,
-                             ],
-                            feed_dict={input_images: batch_images_np, input_sep_synmaps: batch_sep_synmaps_np, input_synmap: batch_synmap_np, input_is_training: True, input_batch_size: configs.train_batch_size})
+                _, \
+                loss,\
+                heatmaps_loss,\
+                fb_loss,\
+                br_loss,\
+                lr,\
+                summary  = sess.run(
+                        [
+                         syn_model.train_op,
+                         syn_model.total_loss,
+                         syn_model.heatmaps_loss,
+                         syn_model.fb_loss,
+                         syn_model.br_loss,
+                         syn_model.lr,
+                         syn_model.merged_summary],
+                        feed_dict={input_images: batch_images_np, input_center_2d: batch_center_2d, input_fb_info: batch_fb_info, input_br_info: batch_br_info, input_is_training: True, input_batch_size: configs.train_batch_size})
+                train_log_writer.add_summary(summary, global_steps)
 
             print("Train Iter:\n" if not is_valid else "Valid Iter:\n")
-            print("Iteration: {:07d} \nlearning_rate: {:07f} \nTotal Loss : {:07f}\nSep synmaps Loss: {:07f}\nSynmap Loss: {:07f}\n\n".format(global_steps, lr, loss, sep_synmaps_loss, synmap_loss))
+            print("Iteration: {:07d} \nlearning_rate: {:07f} \nTotal Loss : {:07f}\nHeatmaps Loss: {:07f}\nFB Loss: {:07f}\nBR Loss: {:07f}\n\n".format(global_steps, lr, loss, heatmaps_loss, fb_loss, br_loss))
             print((len(img_path_for_show) * "{}\n").format(*zip(img_path_for_show, label_path_for_show)))
             print("\n\n")
 
